2177468721b4c9d530f228d20717e51513da0ff0
Peter J. Bjørgmose <petjacmos@gmail.com>
2019-07-12 22:06:18 +0200

    Updates

---
+++ precache-manifest.05ecd715fbd63279800f34cc32303de4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69344cb44f96e0a098f5f09391e6ed4c",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "73e339757b0d5302d589",
+    "url": "/monster-rolodex/static/css/main.3351cb8b.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monster-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "73e339757b0d5302d589",
+    "url": "/monster-rolodex/static/js/main.6a56e13b.chunk.js"
+  },
+  {
+    "revision": "344166cfeaf30c2159bd",
+    "url": "/monster-rolodex/static/js/runtime~main.5df944a8.js"
+  }
+]);
